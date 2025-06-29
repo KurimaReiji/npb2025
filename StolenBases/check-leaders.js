baf3933a98efc769913f523be9f61e48797862a0
Reiji Kurima <kurimareiji@kurimail.com>
2025-06-29 20:51:04 +0900

    add catchers for June 29

--- StolenBases/check-leaders.js
+++ StolenBases/check-leaders.js
@@ -1,7 +1,8 @@
 import { readFileSync } from 'node:fs';
 
 const __dirname = import.meta.dirname;
-const gameDay = (new Intl.DateTimeFormat('en-CA', { timeZone: 'America/Los_Angeles' }).format(new Date()));
+const arg = process.argv.slice(2);
+const gameDay = arg.length > 0 ? arg[0] : (new Intl.DateTimeFormat('en-CA', { timeZone: 'America/Los_Angeles' }).format(new Date()));
 const curLeaders = JSON.parse(readFileSync(`${__dirname}/leaders/${gameDay}.json`, 'utf8'));
 
 const catchers = JSON.parse(readFileSync(`${__dirname}/../docs/npb2025-baserunning-summary.json`, 'utf8')).catchers;
