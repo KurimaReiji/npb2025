0d95acb92dce80d0ad39f8050406d08ba2b5e7ca
Reiji Kurima <kurimareiji@kurimail.com>
2025-07-16 09:52:31 +0900

    move generateStandings to utils.js

--- docs/js/utils.js
+++ docs/js/utils.js
@@ -1,3 +1,5 @@
+import { getTeams } from './npb-teams.js';
+
 /**
  * compare function to sort by winning percentage
  * @param {object} a
@@ -137,6 +139,228 @@ const get_xwl = (win, loss, rs, ra) => {
   };
 };
 
+class AddWinner extends TransformStream {
+  constructor() {
+    super({
+      transform(game, controller) {
+        const sign = Math.sign(game.road.runs - game.home.runs);
+        const winner = [game.home.team, "Tied", game.road.team][sign + 1];
+        const loser = [game.road.team, "Tied", game.home.team][sign + 1];
+
+        const isOneRunGame = Math.abs(game.home.runs - game.road.runs) === 1;
+        const isShutout = game.home.runs === 0 || game.road.runs === 0;
+        const isDoubleDigitRuns = game.home.runs > 9 || game.road.runs > 9;
+        const isExtraInnings = game.isExtraInnings;
+        const isWalkOff = game.isWalkOff;
+        const hadComeback = game.hadComeback;
+
+        const res = Object.assign({}, game, { winner, loser, isOneRunGame, isShutout, isDoubleDigitRuns, isExtraInnings, isWalkOff, hadComeback });
+        controller.enqueue(res);
+      }
+    })
+  }
+}
+
+class SplitHomeRoad extends TransformStream {
+  constructor() {
+    super({
+      transform(game, controller) {
+        ["road", "home"]
+          .map((rh) => {
+            const op = rh === "home" ? "road" : "home";
+            return Object.assign({}, game, {
+              target: game[rh].team,
+              opponent: game[op].team,
+              runsScored: game[rh].runs,
+              runsAllowed: game[op].runs,
+              isVsRHP: game[op].starter === "R",
+              isVsLHP: game[op].starter === "L",
+              gamesPlayed: 1,
+              wins: game.winner === game[rh].team ? 1 : 0,
+              losses: game.loser === game[rh].team ? 1 : 0,
+              ties: game.winner === "Tied" ? 1 : 0,
+              isHome: game[rh].team === game.home.team,
+              isRoad: game[rh].team === game.road.team,
+              isFirstRunScored: game.firstRun === game[rh].team,
+              isFirstRunAllowed: game.firstRun === game[op].team,
+              wlt: game.winner === game[rh].team ? "W" : game.loser === game[rh].team ? "L" : "T",
+            })
+          })
+          .forEach((res) => {
+            controller.enqueue(res);
+          })
+      }
+    })
+  }
+}
+
+async function generateStandings(readable, startDate = '2025-03-28', endDate = '2025-10-24') {
+  function last10(wlt) {
+    const streak = `${wlt.replace(/T/g, "").at(-1)}${wlt.replace(/T/g, "").match(/W+$|L+$/)[0].length}`;
+    const last10 = wlt.slice(-10);
+    const rec = last10.split("")
+      .reduce((a, c) => { a[c] += 1; return a; }, { W: 0, L: 0, T: 0 });
+    return {
+      wins: rec.W,
+      losses: rec.L,
+      ties: rec.T,
+      streak,
+    }
+  }
+
+  const items = {
+    gamesPlayed: 0, wins: 0, losses: 0, ties: 0,
+    runsScored: 0, runsAllowed: 0, runDifferential: "",
+    wlt: "", streak: "",
+    splitRecords: [
+      { type: "left", wins: 0, losses: 0, ties: 0 },
+      { type: "right", wins: 0, losses: 0, ties: 0 },
+      { type: "oneRun", wins: 0, losses: 0 },
+      { type: "shutout", pitching: 0, batting: 0, ties: 0 },
+      { type: "doubleDigitRuns", scored: 0, allowed: 0 },
+      { type: "extraInning", wins: 0, losses: 0, ties: 0 },
+      { type: "walkoff", wins: 0, losses: 0 },
+      { type: "comeback", wins: 0, losses: 0, ties: 0 },
+      { type: "firstRunScored", wins: 0, losses: 0, ties: 0 },
+      { type: "firstRunAllowed", wins: 0, losses: 0, ties: 0 },
+    ],
+  };
+
+  const dataAry = getTeams()
+    .map(({ teamName, league }) => Object.assign({}, { teamName, league }, {
+      overall: structuredClone(items),
+      home: structuredClone(items),
+      road: structuredClone(items),
+    }))
+    .map((o) => [o.teamName, o])
+    ;
+  const data = Object.fromEntries(dataAry);
+
+  for await (const cur of readable) {
+    if (cur.date < startDate) continue;
+    if (cur.date > endDate) continue;
+
+    data[cur.target].lastUpdated = cur.date;
+
+    ["gamesPlayed", "wins", "losses", "ties", "runsScored", "runsAllowed", "wlt"].forEach((wlt) => {
+      data[cur.target].overall[wlt] += cur[wlt];
+      if (cur.isHome) data[cur.target].home[wlt] += cur[wlt];
+      if (cur.isRoad) data[cur.target].road[wlt] += cur[wlt];
+    });
+
+    [
+      { criteria: cur.isOneRunGame, item: "oneRun", skipTies: true },
+      { criteria: cur.isVsRHP, item: "right" },
+      { criteria: cur.isVsLHP, item: "left" },
+      { criteria: cur.isExtraInnings, item: "extraInning" },
+      { criteria: cur.isWalkOff, item: "walkoff", skipTies: true },
+      { criteria: cur.hadComeback, item: "comeback" },
+      { criteria: cur.isFirstRunScored, item: "firstRunScored" },
+      { criteria: cur.isFirstRunAllowed, item: "firstRunAllowed" },
+    ].forEach(({ criteria, item, skipTies }) => {
+      if (criteria) {
+        ["wins", "losses", "ties"].forEach((wlt) => {
+          if (wlt === "ties" && skipTies) return;
+          data[cur.target].overall.splitRecords.find(sp => sp.type === item)[wlt] += cur[wlt];
+          if (cur.isHome) data[cur.target].home.splitRecords.find(sp => sp.type === item)[wlt] += cur[wlt];
+          if (cur.isRoad) data[cur.target].road.splitRecords.find(sp => sp.type === item)[wlt] += cur[wlt];
+        });
+      }
+    });
+
+    if (cur.isShutout) {
+      const pitching = cur.runsAllowed === 0 ? 1 : 0;
+      const batting = cur.runsScored === 0 ? 1 : 0;
+      const ties = cur.runsAllowed === 0 && cur.runsScored === 0 ? 1 : 0;
+      const item = "shutout";
+      data[cur.target].overall.splitRecords.find(sp => sp.type === item).pitching += pitching;
+      data[cur.target].overall.splitRecords.find(sp => sp.type === item).batting += batting;
+      data[cur.target].overall.splitRecords.find(sp => sp.type === item).ties += ties;
+      if (cur.isHome) {
+        data[cur.target].home.splitRecords.find(sp => sp.type === item).pitching += pitching;
+        data[cur.target].home.splitRecords.find(sp => sp.type === item).batting += batting;
+        data[cur.target].home.splitRecords.find(sp => sp.type === item).ties += ties;
+      }
+      if (cur.isRoad) {
+        data[cur.target].road.splitRecords.find(sp => sp.type === item).pitching += pitching;
+        data[cur.target].road.splitRecords.find(sp => sp.type === item).batting += batting;
+        data[cur.target].road.splitRecords.find(sp => sp.type === item).ties += ties;
+      }
+    }
+    if (cur.isDoubleDigitRuns) {
+      const scored = cur.runsScored > 9 ? 1 : 0;
+      const allowed = cur.runsAllowed > 9 ? 1 : 0;
+      const item = "doubleDigitRuns";
+      data[cur.target].overall.splitRecords.find(sp => sp.type === item).scored += scored;
+      data[cur.target].overall.splitRecords.find(sp => sp.type === item).allowed += allowed;
+      if (cur.isHome) {
+        data[cur.target].home.splitRecords.find(sp => sp.type === item).scored += scored;
+        data[cur.target].home.splitRecords.find(sp => sp.type === item).allowed += allowed;
+      }
+      if (cur.isRoad) {
+        data[cur.target].road.splitRecords.find(sp => sp.type === item).scored += scored;
+        data[cur.target].road.splitRecords.find(sp => sp.type === item).allowed += allowed;
+      }
+    }
+  }
+
+  const npb = Object.values(data)
+    .map((o) => Object.assign({
+      teamName: o.teamName,
+      league: o.league,
+      wins: o.overall.wins,
+      losses: o.overall.losses,
+      ties: o.overall.ties,
+      pct: winpct(o.overall.wins, o.overall.losses).toFixed(3).replace(/^0/, "")
+    }, o))
+    .map((o) => {
+      ["overall", "home", "road"].forEach((key) => {
+        o[key].runDifferential = o[key].runsScored - o[key].runsAllowed;
+        o[key].splitRecords.push({
+          type: "xWinLoss",
+          ...get_xwl(o[key].wins, o[key].losses, o[key].runsScored, o[key].runsAllowed)
+        });
+        const { streak, ...rest } = last10(o[key].wlt)
+        Object.assign(o[key], { lastTen: rest, streak });
+      });
+      return o;
+    })
+    ;
+
+  const cl = npb.filter((o) => o.league === "Central")
+    .sort(teams_by_wpct)
+    .map((o, i, ary) => {
+      o.overall.gamesBack = i === 0 ? "" : games_behind(o.overall.wins, o.overall.losses, ary[0].overall.wins, ary[0].overall.losses);
+      return o;
+    });
+  const pl = npb.filter((o) => o.league === "Pacific")
+    .sort(teams_by_wpct)
+    .map((o, i, ary) => {
+      o.overall.gamesBack = i === 0 ? "" : games_behind(o.overall.wins, o.overall.losses, ary[0].overall.wins, ary[0].overall.losses);
+      return o;
+    });
+
+  const json = {
+    "records": [
+      {
+        standingsType: "regular season",
+        season: "2025",
+        league: "Central League",
+        lastUpdated: cl.map(o => o.lastUpdated).sort().at(-1),
+        teamRecords: cl,
+      },
+      {
+        standingsType: "regular season",
+        season: "2025",
+        league: "Pacific League",
+        lastUpdated: pl.map(o => o.lastUpdated).sort().at(-1),
+        teamRecords: pl,
+      }
+    ]
+  };
+  return json;
+}
+
 export {
   teams_by_wpct,
   winpct,
@@ -146,4 +370,7 @@ export {
   get_venues,
   to_uniq,
   get_xwl,
+  AddWinner,
+  SplitHomeRoad,
+  generateStandings,
 }
